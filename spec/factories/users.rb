5d73bbccd7ef2b54a7b289029ff4b3cdefa3592e
Padrig Tuck <Padrig@gmail.com>
2016-07-18 12:31:51 -0400

    devise w/ hound config

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |num| "test#{num}@gmail.com" }
+    sequence(:username) { |num| "test#{num}" }
+    password "test1234"
   end
 end
