941bca71e12a4948a963a808c18fd4c8644d041d
Padrig Tuck <Padrig@gmail.com>
2016-07-18 11:59:36 -0400

    devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+  validates :encrypted_password, presence: true
+end
