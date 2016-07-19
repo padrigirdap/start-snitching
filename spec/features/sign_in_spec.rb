e050f4b08027490c6eada6f13ede4cd029262659
Padrig Tuck <Padrig@gmail.com>
2016-07-18 23:32:51 -0400

    sign in/out tests

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,6 +6,7 @@ feature "users can sign in" do
   scenario "user visits root_path and signs in successfully" do
     sign_in
     expect(page).to have_content("Signed in successfully.")
+    expect(page).to have_content("Sign Out")
   end
 
   scenario "user provides invalid credentials and fails to sign in" do
@@ -14,5 +15,6 @@ feature "users can sign in" do
     fill_in "Username", with: "top"
     click_button "Log in"
     expect(page).to have_content("Invalid Username or password.")
+    expect(page).to have_content("Sign In")
   end
 end
